6f42587ff2dcccfda43c835293a5dca0d4b5dc3a
Adrian <adrianml333@gmail.com>
2018-03-02 14:26:35 -0600

    Clase Enemigo

---
+++ core/src/mx/itesm/another_monkey_paradox/Enemigo.java
@@ -0,0 +1,35 @@
+package mx.itesm.another_monkey_paradox;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+/**
+ * Created by Adrian on 02/03/18.
+ */
+
+class Enemigo {
+
+    private Animation animacion;
+    private float x, y; // Coordenadas
+    private float timerAnimacion;
+
+    public Enemigo(Texture texture1, Texture texture2, Texture texture3, Texture texture4){
+        TextureRegion img1 = new TextureRegion(texture1);
+        TextureRegion img2 = new TextureRegion(texture2);
+        TextureRegion img3 = new TextureRegion(texture3);
+        TextureRegion img4 = new TextureRegion(texture4);
+        animacion = new Animation(0.2f, img1, img2, img3, img4);
+        x = PantallaJuego.ANCHO/2-texture1.getWidth()/2;
+        y = PantallaJuego.ALTO/2-texture1.getHeight()/2;
+        animacion.setPlayMode(Animation.PlayMode.LOOP);
+    }
+
+    public void render(SpriteBatch batch){
+        timerAnimacion += Gdx.graphics.getDeltaTime();
+        TextureRegion frame = (TextureRegion) animacion.getKeyFrame(timerAnimacion);
+        batch.draw(frame, x, y);
+    }
+}
