92ce021231c6b5fa3e85a84a4741d3d721b23adc
luisferced <fernandocm_97@outlook.com>
2018-03-20 14:02:58 -0600

    Cambio de velocidad de Astro en PantallaJuego

--- core/src/mx/itesm/another_monkey_paradox/PantallaJuego.java
+++ core/src/mx/itesm/another_monkey_paradox/PantallaJuego.java
@@ -209,8 +209,6 @@ class PantallaJuego extends Pantalla implements Screen  {
             public void clicked(InputEvent event, float x, float y) {
                 super.clicked(event, x, y);
                 //Gdx.app.log("ClickListener","Si se clickeoooo");
-                dispose();
-                main.setScreen(new PantallaJuego(main));
             }
         });
 
@@ -219,8 +217,6 @@ class PantallaJuego extends Pantalla implements Screen  {
             public void clicked(InputEvent event, float x, float y) {
                 super.clicked(event, x, y);
                 //Gdx.app.log("ClickListener","Si se clickeoooo");
-                dispose();
-                main.setScreen(new PantallaJuego(main));
             }
         });
 
