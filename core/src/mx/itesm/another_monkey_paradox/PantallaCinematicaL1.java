602e773648ca616bb92fd5ca34542590e982caca
AdrianML333 <adrianml333@gmail.com>
2018-04-03 17:57:40 -0500

    Pantalla Victoria corregida y PantallaCinemaLevel1

---
+++ core/src/mx/itesm/another_monkey_paradox/PantallaCinematicaL1.java
@@ -0,0 +1,132 @@
+package mx.itesm.another_monkey_paradox;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.Scaling;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+/**
+ * Created by adrian on 03/04/2018.
+ */
+
+class PantallaCinematicaL1 implements Screen {
+
+    private final Main main;
+
+    public static final float ANCHO = 1280;
+    public static final float ALTO = 780;
+
+    //Camara
+    private OrthographicCamera camara;
+    private Viewport vista;
+    //Escena
+    private Stage stageMenu;
+
+    private SpriteBatch batch;
+
+    //For Background
+    Texture imgBackground;
+    private Sprite spriteBackground;
+
+    //background music
+    private Music musicMenu = Gdx.audio.newMusic(Gdx.files.internal("loboloco.mp3"));
+
+    public PantallaCinematicaL1(Main main) {
+        this.main = main;
+    }
+
+    @Override
+    public void show() {
+        crearCamara();
+        crearMenu();
+        batch = new SpriteBatch();
+        musicMenu.setLooping(true);
+        //musicMenu.play();
+    }
+
+    private void crearMenu() {
+        stageMenu = new Stage(vista);
+
+        imgBackground = new Texture("cineL1.png");
+        spriteBackground = new Sprite(imgBackground);
+
+        //Boton Next
+        TextureRegionDrawable trdNext = new TextureRegionDrawable(new TextureRegion(new Texture("PlayButton.png")));
+        ImageButton btnNext = new ImageButton(trdNext);
+        btnNext.setSize(150,150);
+        btnNext.setPosition(ANCHO*1/10, ALTO*1/10);
+        btnNext.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                super.clicked(event, x, y);
+                //Gdx.app.log("ClickListener","Si se clickeoooo");
+                main.setScreen(new PantallaMenu(main));
+            }
+        });
+
+
+        stageMenu.addActor(btnNext);
+
+        Gdx.input.setInputProcessor(stageMenu);
+}
+
+    private void crearCamara(){
+        camara = new OrthographicCamera(ANCHO,ALTO);
+        camara.position.set(ANCHO/2, ALTO/2,0);
+        camara.update();
+        vista = new StretchViewport(ANCHO,ALTO,camara);
+    }
+
+    @Override
+    public void render(float delta) {
+        //Usar v=d/t o en este caso d=v*t
+        Gdx.gl.glClearColor(.1f,.4f,.9f,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        batch.setProjectionMatrix(camara.combined);
+        batch.begin();
+        spriteBackground.draw(batch);
+        batch.end();
+        stageMenu.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        vista.update(width,height);
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+}
